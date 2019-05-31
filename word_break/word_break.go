96cbb76585f61c2f6a79c17300477c0fcf3484d5
Tudor P <tudorxp@gmail.com>
2019-05-31 22:07:53 +0100

    more things

--- word_break/word_break.go
+++ word_break/word_break.go
@@ -14,13 +14,58 @@ func die_if_error(err error) {
 	}
 }
 
-func word_break(s []rune, dictionary map[string]bool) {
+// word_break takes arguments: a string as a slice of runes, and a dictionary
+// it returns true, space separated words if the input string can be broken into words from the dictionary
+// or false, empty string if not
+func word_break(s []rune, dictionary map[string]bool) (bool, string) {
+
+	// a[i].possible is true if for a specific index i, s[0:i+1] is word breakable
+	// a[i].word is the (last) word that made it true
 	var a []struct{
 		possible bool
 		word string
 	}
 	a=make([]struct {possible bool; word string}, len(s))
-	_=a
+
+	// l is a map which stores all values of i where a[i]==true
+	l:=make(map[int]bool)
+	l[-1]=true // because null substring is true
+
+	for i:=0; i<len(s); i++ {
+		if a[i].possible != true {
+			for j, _ := range l { // check substrings from prefixes which matched
+				if dictionary[string(s[j+1:i+1])] {
+					a[i].possible=true
+					a[i].word=string(s[j+1:i+1])
+					l[i]=true
+					// fmt.Printf("Found match %d:%d: %s\n",j+1,i+1,string(s[j+1:i+1]))
+					break
+				}
+			}
+		}
+	}
+
+	if len(s)==0 {
+		// fmt.Printf("Word break possible: empty string\n")
+		return true, ""
+	}
+	if a[len(s)-1].possible==true {
+		// fmt.Printf("Word break possible\n")
+		// compose string
+		words:=[]string{}
+		for i:=len(s)-1;i>=0; {
+			words=append(words,a[i].word)
+			i-=len(a[i].word)
+		}
+		str:=""
+		for i:=len(words)-1;i>=0;i-- { 
+			str+=words[i]+" "
+		}
+		// fmt.Println(str)
+		return true, str
+	}
+	return false, ""
+
 }
 
 func main () {
@@ -59,6 +104,11 @@ func main () {
 
 	// fmt.Println("Got: ",string(s))
 
-	word_break(s,dictionary)
+	possible, str := word_break(s,dictionary)
+	if possible {
+		fmt.Println("Word break possible: ",str)
+	} else {
+		fmt.Println("Word break NOT possible")
+	}
 
 }
